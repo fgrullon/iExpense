56eac892e38144830406c0164852cc4c2d76c07d
fgrullon <franfelix@gmail.com>
2024-08-17 10:57:50 -0400

    Add dynamic list and the funtionality to add new numbers as well as delete from the list

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -11,15 +11,42 @@ struct SecondView: View {
     @Environment(\.dismiss) var dismiss
     
     let name: String
-    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     var body: some View{
+        
         Text("Second View")
         Text("Hello, \(name)")
         Button("Dismiss"){
             dismiss()
         }
+        NavigationStack{
+            VStack{
+                List{
+                    ForEach(numbers, id: \.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number"){
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar{
+                EditButton()
+            }
+        }
+
     }
+    
+    func removeRows(at offSets: IndexSet){
+        numbers.remove(atOffsets : offSets)
+    }
+
 }
+
 
 struct ContentView: View {
     @State private var showingSheet = false
@@ -34,6 +61,8 @@ struct ContentView: View {
         }
     }
 }
+
+
 
 #Preview {
     ContentView()

9f6db4eb8ea9fdd35f2561602ef030c0589fd261
fgrullon <franfelix@gmail.com>
2024-08-19 19:21:21 -0400

    Persist data json encode data in userDefault and load on start

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Frank Grullon on 19/8/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
